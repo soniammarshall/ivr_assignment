added new fk calculation (not correct)

src/joint_angles_estimator.py
@@ -51,7 +51,11 @@ class joint_angles_estimator:
         blue_to_green = blue_to_green / np.linalg.norm(blue_to_green)
 
         # return error which is to be minimized
-        return abs(c1 * c3 + s1 * s2 * s3 - blue_to_green[0]) + abs(s1 * c3 - c1 * s2 * s3 - blue_to_green[1]) + abs(c2 * s3 - blue_to_green[2])
+
+        # to be used with first set of DH params
+        # return abs(c1 * c3 + s1 * s2 * s3 - blue_to_green[0]) + abs(s1 * c3 - c1 * s2 * s3 - blue_to_green[1]) + abs(c2 * s3 - blue_to_green[2])
+        # to be used with second set of DH params
+        return abs(c1 * c2 * c3 - s1 * s3 - blue_to_green[0]) + abs(s1 * c2 * c3 + c1 * s3 - blue_to_green[1]) + abs(-(s2 * c3) - blue_to_green[2])
 
     def measure_angles(self):
         # Descends in the angle space towards the minimum error, minimizing the error function and finding theta 1, 2 and 3